1e68ce81d6aef412423e171b8edf2061c249cb80
ghanicodes <abdulghaniag1010@gmail.com>
2026-04-01 16:07:39 +0500

    login error resolve

--- server.js
+++ server.js
@@ -14,8 +14,19 @@ import addHomeDelivery  from "./src/router/addHomeDeliveryRouter.js";
 const app = express();
 dotenv.config();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL
+].filter(Boolean);
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 app.use(express.json());
